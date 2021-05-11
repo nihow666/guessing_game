9cb804b34e12a3a79a03062e4cb541e90c00e738
solomeowl <j19940430@gmail.com>
2021-05-12 02:01:42 +0800

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     println!("請猜測一個數字！");
-    println!("請輸入你的猜測數字：");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("讀取該行失敗");
-    println!("你的猜測數字：：{}", guess)
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop {
+        println!("請輸入你的猜測數字：");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("讀取該行失敗");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了！\n----------------"),
+            Ordering::Greater => println!("太大了！\n----------------"),
+            Ordering::Equal => {
+                print!("答對了！\n----------------");
+                break;
+            }
+        }
+    }
 }
